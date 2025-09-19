671b5fe15d6b25197b8f121a0b963b733c4826e5
Brian Soh <brian.soh.cs@gmail.com>
2025-09-19 15:09:16 -0700

    Initial Commit

---
+++ Face-Recognition-Boarding-System/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Face-Recognition-Boarding-System
+//
+//  Created by Brian Soh on 2025-09-19.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
